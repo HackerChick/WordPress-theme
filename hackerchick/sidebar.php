739a1b9602c66e3991ef508ea86b3e6dc06f3632
Abby Fichtner <haxrchick@gmail.com>
2012-12-28 23:28:33 -0500

    changed styles of post text, mostly to be smaller, remove italics

--- hackerchick/sidebar.php
+++ hackerchick/sidebar.php
@@ -72,6 +72,7 @@
     <!-- BADGES -->
     <div class="item" id="badges">
     <div id="badge-topblogs"><a href="http://agilescout.com/top-agile-blogs-200/" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/images/agile-top-200-blogs.png" width="100" height="94" alt="Top 200 Agile Blogs" border="0"/></a></div>
+    <div id="badge-topwomen"><a href="http://agilescout.com/woman-in-leadership-top-women-in-agile-thought-leadership/" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/images/agile-top-woman-list.png" width="100" height="100" alt="Top 200 Agile Blogs" border="0"/></a></div>
     <div id="badge-agile11"><a href="http://program2011.agilealliance.org/event/bbbd058a97e906ab96d85c0aa712a751" title="Lean Startup: How Development Looks Different When You're Changing the World" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/images/badge-agile2011.png" width="150" height="68" alt="Agile 2011" border="0"/></a></div>
     <div id="badge-geekgirl"><a href="http://www.geekgirlblogs.com/" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/images/badge-geekgirlblogs.png" width="142" height="50" alt="Geek Girl Blogs" border="0"/></a></div>
     <div id="badge-devchix"><a href="http://www.devchix.com/" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/images/badge-devchix.png" width="69" height="75" alt="DevChix" border="0"/></a></div>
